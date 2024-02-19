d50c539dbfd2014d4ede3ed9d10cfc8b14cda650
kenja <pi.sumet_st@tni.ac.th>
2024-02-20 00:03:22 +0700

    added tap and 3 pages

---
+++ lib/pages/fastfood_page.dart
@@ -0,0 +1,18 @@
+import 'package:calcal/reuse.dart';
+import 'package:flutter/material.dart';
+
+class fastfood_page extends StatelessWidget {
+  const fastfood_page({super.key});
+
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+          backgroundImg(),
+
+          //
+        ],
+      ),
+    );
+  }
+}
